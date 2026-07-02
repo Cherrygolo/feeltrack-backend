530a00897db2a944ee5ab998cf337b5daa166dca
Louise Delbecque <delbecquelouise@gmail.com>
2026-07-02 17:09:25 +0200

    🔧🐛fix(SentimentAnalysisApiConfig): update huggingface token management to run app sucessfully with null by default

--- src/main/java/ld/feeltrack_backend/external/nlp/SentimentAnalysisApiConfig.java
+++ src/main/java/ld/feeltrack_backend/external/nlp/SentimentAnalysisApiConfig.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class SentimentAnalysisApiConfig {
 
-    @Value("${huggingface.token}")
+    @Value("${HUGGINGFACE_TOKEN:#{null}}")
     private String token;
 
     @Bean
