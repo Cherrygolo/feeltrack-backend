39d6231ddbaabe7ea6216e0748cc8192acafd3e8
Louise Delbecque <delbecquelouise@gmail.com>
2026-07-07 17:08:08 +0200

    ✨feat(HomeController): add API endpoint for application status

---
+++ src/main/java/ld/feeltrack_backend/controller/HomeController.java
@@ -0,0 +1,20 @@
+package ld.feeltrack_backend.controller;
+
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/api")
+    public Map<String, String> api() {
+        return Map.of(
+                "application", "FeelTrack API",
+                "status", "UP",
+                "version", "1.0"
+        );
+    }
+
+}
