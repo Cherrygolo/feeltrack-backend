73b0567bc92e9b0950cca5ae6cfb42428d74a4c8
Louise Delbecque <delbecquelouise@gmail.com>
2026-07-07 17:40:59 +0200

    ♻️ref(HomeController): update API endpoint mapping to match global api base path prefix

--- src/main/java/ld/feeltrack_backend/controller/HomeController.java
+++ src/main/java/ld/feeltrack_backend/controller/HomeController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HomeController {
 
-     @GetMapping({"/api", "/api/"})
+     @GetMapping({"", "/"})
     public Map<String, String> api() {
         return Map.of(
                 "application", "FeelTrack API",
